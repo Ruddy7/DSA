311527fa2a025e50d2ff622c2035aff01fc578fc
Rudresh Bhargav <rudreshbhargav333@gmail.com>
2025-10-09 00:01:32 +0530

    Easy Problems

---
+++ LinkedList/EasyProblems/reverseSingly.java
@@ -0,0 +1,34 @@
+public class reverseSingly {
+
+    static Node reverse(Node head) {
+        Node curr = head;
+        Node next;
+        Node prev = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    static void printList(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        printList(head);
+        head = reverse(head);
+        printList(head);
+    }
+}
